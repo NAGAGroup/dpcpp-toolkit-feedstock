480e24a812bb501ddb8d7f2e68ef1833a862dd7c
Jack Myers <jackhyers97@gmail.com>
2024-12-08 19:13:47 -0800

    WIP
    Signed-off-by: Jack Myers <jackhyers97@gmail.com>

--- recipe/activate.sh
+++ recipe/activate.sh
@@ -1,11 +1,17 @@
+if [ -n "${CONDA_CUDA_ROOT}" ]; then
+  export CONDA_BACKUP_CUDA_ROOT="$CONDA_CUDA_ROOT"
+fi
+if [ -n "${CUDA_LIB_PATH}" ]; then
+  export CONDA_BACKUP_CUDA_LIB_PATH="$CUDA_LIB_PATH"
+fi
 export CONDA_CUDA_ROOT="$PREFIX/targets/x86_64-linux"
 export CUDA_LIB_PATH="$CONDA_CUDA_ROOT/lib/stubs"
 
 if [ "${CONDA_BUILD:-0}" = "1" ]; then
   export CC="${BUILD_PREFIX}/bin/clang"
   export CXX="${BUILD_PREFIX}/bin/clang++"
-  echo "--cuda-path=${CUDA_CONDA_ROOT} --sysroot=${CONDA_BUILD_SYSROOT}" >${BUILD_PREFIX}/bin/${CONDA_TOOLCHAIN_HOST}-clang.cfg
-  echo "--cuda-path=${CUDA_CONDA_ROOT} --sysroot=${CONDA_BUILD_SYSROOT}" >${BUILD_PREFIX}/bin/${CONDA_TOOLCHAIN_HOST}-clang++.cfg
+  echo "--cuda-path=${CUDA_CONDA_ROOT} --sysroot=${CONDA_BUILD_SYSROOT}" >${PREFIX}/bin/${CONDA_TOOLCHAIN_HOST}-clang.cfg
+  echo "--cuda-path=${CUDA_CONDA_ROOT} --sysroot=${CONDA_BUILD_SYSROOT}" >${PREFIX}/bin/${CONDA_TOOLCHAIN_HOST}-clang++.cfg
 else
   export CC="${CONDA_PREFIX}/bin/clang"
   export CXX="${CONDA_PREFIX}/bin/clang++"
--- recipe/build.sh
+++ recipe/build.sh
@@ -12,9 +12,8 @@ bash "${SUBPROJECT_ROOT}/scripts/install.sh"
 
 # Copy the [de]activate scripts to $PREFIX/etc/conda/[de]activate.d.
 # This will allow them to be run on environment activation.
-CHANGE="~activate-dpcpp-clang"
-mkdir -p "${PREFIX}/etc/conda/activate.d"
-cp "${RECIPE_DIR}/${CHANGE}.sh" "${PREFIX}/etc/conda/activate.d/${CHANGE}.sh"
-mkdir -p "${PREFIX}/etc/conda/deactivate.d"
-CHANGE="~deactivate-dpcpp-clang"
-touch "${PREFIX}/etc/conda/deactivate.d/${CHANGE}.sh"
+CHANGE="activate"
+for CHANGE in "activate" "deactivate"; do
+  mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
+  cp "${RECIPE_DIR}/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/~open-dpcpp-toolkit-${CHANGE}.sh"
+done
